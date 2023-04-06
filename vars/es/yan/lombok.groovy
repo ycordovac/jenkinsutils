d79278ff842adafe145e227e04776fb829e3ce78
ycordovac <117431970+ycordovac@users.noreply.github.com>
2023-04-06 18:42:12 +0200

    subida de libreria

--- vars/es/yan/lombok.groovy
+++ vars/es/yan/lombok.groovy
@@ -3,5 +3,5 @@ import es.yan.CommandWrapper
 def lombokCall(){
     // CommandWrapper grapper=new CommandWrapper("ln -ls");
     // grapper.executeCommand()
-    println("from lombok")
+    sh "pwd"
 }
