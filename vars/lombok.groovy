d2a40064833c30c74d4318ba5cae0b1211f7f0c4
ycordovac <117431970+ycordovac@users.noreply.github.com>
2023-04-10 04:19:30 +0200

    subida de libreria

--- vars/lombok.groovy
+++ vars/lombok.groovy
@@ -1,5 +1,5 @@
 import es.yan.CommandWrapper
 def call(){
-    CommandWrapper grapper=new CommandWrapper("ln -ls");
-    grapper.executeCommand()
+    CommandWrapper grapper=new CommandWrapper("ln -ls", false);
+    return grapper.executeCommand()
 }
