4fb6ac245c8d9394f6b3a448f0da7f5c396d5a60
ycordovac <117431970+ycordovac@users.noreply.github.com>
2023-04-11 12:45:13 +0200

    subida de libreria

--- src/es/yan/CommandWrapper.groovy
+++ src/es/yan/CommandWrapper.groovy
@@ -19,9 +19,9 @@ class CommandWrapper{
         
     }
 
-    // String getMetadataCommand(){
-    //     return metadataCommand
-    // }
+    String getMetadataCommand(){
+        return metadataCommand
+    }
 
     boolean executeCommandWaitAndFillValues(String commandtoExecute){
         this.agent=env.NODE_NAME
